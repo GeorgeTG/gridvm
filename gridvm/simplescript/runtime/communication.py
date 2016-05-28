20212c30fd7cdca00d55104935f479ba9dc26f95
Konstantinos Kanellis <kkanelli@hotmail.com>
2016-05-28 21:15:55 +0300

    minor bugfixes - local programs are working

--- gridvm/simplescript/runtime/communication.py
+++ gridvm/simplescript/runtime/communication.py
@@ -1,4 +1,5 @@
 from queue import Queue, Empty
+from threading import Thread
 
 from gridvm.network.nethandler import NetHandler
 from gridvm.network.protocol.packet import PacketType
@@ -9,7 +10,7 @@ class EchoCommunication:
     def __init__(self, *args, **kwargs):
         self._messages = {}
 
-    def recv(self, who):
+    def receive_message(self, who):
         queue = self._messages.setdefault(who, list())
 
         try:
@@ -17,20 +18,20 @@ class EchoCommunication:
         except IndexError:
             return None
 
-    def snd(self, to, what):
+    def send_message(self, to, what):
         queue = self._messages.setdefault(to, list())
         queue.insert(0, what)
 
-    def can_recv(self, who):
+    def can_receive_message(self, who):
         queue = self._messages.setdefault(who, list())
         return len(queue) > 0
-
 
     def shutdown(self):
         pass
 
 class NetworkCommunication:
-    def __init__(self, net_interface):
+    def __init__(self, runtime_id, net_interface):
+        self.runtime_id = runtime_id
         self._messages = { }    # Messages that have arrived for threads
         self._fwd_table = { }   # Forwarding table <pid, tid> -> <runtime_id>
 
@@ -38,7 +39,11 @@ class NetworkCommunication:
         self._status_req = Queue()
 
         self._to_send = Queue() # Packets that should be send over network (runtime_id, packet)
-        self.nethandler = NetHandler(self, net_interface)
+        self.nethandler = NetHandler(self, runtime_id, net_interface)
+
+        # Start NetHandler
+        self.nethandler_thread = Thread(target=self.nethandler.start)
+        self.nethandler_thread.start()
 
     def receive_message(self, thread_uid):
         """ Called from Runtime to receive a message destined for a thread
@@ -73,17 +78,26 @@ class NetworkCommunication:
         """
         runtime_id = self._get_runtime_id(thread_uid)
 
-        packet = make_packet(
-            PacketType.THREAD_MESSAGE,
-            thread_uid=thread_uid,
-            msg=msg
-        )
-        self._to_send.put( (runtime_id, packet) )
+        if runtime_id == self.runtime_id:
+            # Simply add to local message queue
+            queue = self._messages.setdefault(thread_uid, Queue())
+            queue.put(msg)
+        else:
+            packet = make_packet(
+                PacketType.THREAD_MESSAGE,
+                thread_uid=thread_uid,
+                msg=msg
+            )
+            self._to_send.put( (runtime_id, packet) )
 
     def add_thread_message(self, packet):
         """ Called from NetHandler to add a new thread message which has arrived """
         thread_uid, msg = packet['thread_uid'], packet['msg']
-        self._messages[thread_uid].put(msg)
+
+        # Add the message to the queue
+        queue = self._messages.setdefault(thread_uid, Queue())
+        queue.put(msg)
+
 
 
     def get_print_requests(self):
@@ -112,6 +126,8 @@ class NetworkCommunication:
         thread_uid, msg = packet['thread_uid'], packet['msg']
         self._print_req.put( (thread_uid, msg))
 
+
+
     def get_status_requests(self):
         """ Called from Runtime to get a list of thread status changes of its own threads """
         return self._get_list( self._status_req )
@@ -134,6 +150,8 @@ class NetworkCommunication:
         thread_uid, status = packet['thread_uid'], packet['status']
         self._status_req.put( (thread_uid, status) )
 
+
+
     def update_thread_location(self, thread_uid, new_location):
         """ Called from NetHandler once a MIGRATION_COMPLETED packet has been received
 
@@ -145,7 +163,7 @@ class NetworkCommunication:
 
     def get_to_send_requests(self):
         """ Called from NetHandler """
-        return self._get_list( self._status_req )
+        return self._get_list( self._to_send )
 
     def shutdown(self):
         # TODO: move all foreign threads to other runtimes
@@ -166,7 +184,7 @@ class NetworkCommunication:
         """ Return the id of the runtime that currently runs this thread """
         if thread_uid not in self._fwd_table:
             # TODO: Broadcast message to discover the location of the thread
-            print('ERROR: {} not in fwd table..'.format(thread_uid))
+            raise RuntimeError('ERROR: {} not in fwd table..'.format(thread_uid))
             return None
 
         return self._fwd_table[thread_uid]
