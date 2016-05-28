c54757f37ccffe04d79f54c713338f8647e1f757
George T. Gougoudis <george.gougoudis@gmail.com>
2016-05-29 02:13:59 +0300

    add netutils

---
+++ gridvm/network/utils.py
@@ -0,0 +1,12 @@
+import socket
+import fcntl
+import struct
+
+def get_if_address(ifname):
+    # Deep Black Magic
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15].encode('utf-8'))
+    )[20:24])
