46c1e0ee38439cf2fd5ee99c48d7fc5d9fae5f08
Konstantinos Kanellis <kkanelli@hotmail.com>
2016-05-28 15:30:40 +0300

    minor naming changes

--- gridvm/simplescript/runtime/communication.py
+++ gridvm/simplescript/runtime/communication.py
@@ -1,5 +1,6 @@
 from queue import Queue, Empty
 
+from gridvm.network.nethandler import NetHandler
 from gridvm.network.protocol.packet.ptype import PacketType
 from gridvm.network.protocol.packet.packet import Packet
 from gridvm.network.protocol.packet.factory import make_packet
@@ -25,7 +26,7 @@ class EchoCommunication:
         return len(queue) > 0
 
 class NetworkCommunication:
-    def __init__(self):
+    def __init__(self, net_interface):
         self._messages = { }    # Messages that have arrived for threads
         self._fwd_table = { }   # Forwarding table <pid, tid> -> <runtime_id>
 
@@ -33,61 +34,70 @@ class NetworkCommunication:
         self._status_req = Queue()
 
         self._to_send = Queue() # Packets that should be send over network (runtime_id, packet)
-        self.net_handler = NetHandler(self)
+        self.nethandler = NetHandler(self, net_interface)
 
-    def receive_message(self, who):
-        """ Called from Runtime to receive a message destined for a thread """
+    def receive_message(self, thread_uid):
+        """ Called from Runtime to receive a message destined for a thread
+
+        Parameters:
+            -- thread_uid: (program_id, thread_id)
+        """
+
         # Create queue if not exist
-        queue = self._messages.setdefault(who, Queue())
+        queue = self._messages.setdefault(thread_uid, Queue())
 
         try:
             return queue.get(block=False)
         except Empty:
             return None
 
-    def can_receive_message(self, who):
-        """ Called from Runtime to check if a message is pending for a thread """
-        queue = self._messages.setdefault(who, Queue())
+    def can_receive_message(self, thread_uid):
+        """ Called from Runtime to check if a message is pending for a thread
+
+        Parameters:
+            -- thread_uid: (program_id, thread_id)
+        """
+        queue = self._messages.setdefault(thread_uid, Queue())
         return not queue.empty()
 
-    def send_message(self, to, what):
+    def send_message(self, thread_uid, msg):
         """ Called from Runtime to send a message to another thread (same program)
 
             Parameters:
-                -- to:      (program_id, thread_id) tuple
-                -- what:    message to send
+                -- thread_uid:  (program_id, thread_id)
+                -- msg:         message to send
         """
-        runtime_id = self._get_runtime_id(to)
+        runtime_id = self._get_runtime_id(thread_uid)
 
         packet = make_packet(
             PacketType.THREAD_MESSAGE,
-            thread_id=to,
-            payload=what
+            thread_uid=thread_uid,
+            payload=msg
         )
         self._to_send.put( (runtime_id, packet) )
-
 
     def get_print_requests(self):
         """ Called from Runtime to get a list of print requests for its own threads """
         return self._get_list( self._print_req )
 
-    def send_print_request(self, to, msg):
+    def send_print_request(self, thread_uid, msg):
         """ Called from Runtime to send a print request to responsible runtime
 
             Parameters:
-                -- to:      (program_id, thread_id) tuple
-                -- what:    message to print
+                -- thread_uid:  (program_id, thread_id)
+                -- msg:         message to print
         """
+
         # TODO: find a way to get original runtime id
         #runtime_id = self._get_runtime_id(who)
 
         """
         packet = make_packet(
             PacketType.RUNTIME_PRINT_REQ,
-            thread_id=who,
+            thread_uid=thread_uid,
             msg=msg
         )
-        self._to_send.put( (who, packet) )
+        self._to_send.put( (runtime_id, packet) )
         """
         pass
 
@@ -95,12 +105,11 @@ class NetworkCommunication:
         """ Called from NetHandler to add a print request which has arrived """
         pass
 
-
     def get_status_requests(self):
         """ Called from Runtime to get a list of thread status changes of its own threads """
         return self._get_list( self._status_req )
 
-    def send_status_request(self, who, new_status):
+    def send_status_request(self, thread_uid, new_status):
         """ Called from Runtime to notify the thread's responsible runtime, for
             a thread status change """
 
@@ -112,36 +121,38 @@ class NetworkCommunication:
         pass
 
 
-    def update_thread_location(self, thread_id, new_location):
+    def update_thread_location(self, thread_uid, new_location):
         """ Called from NetHandler once a MIGRATION_COMPLETED packet has been received
 
         Paramters:
-            -- thread_id:       (program_id, thread_id)
+            -- thread_uid:       (program_id, thread_id)
             -- new_location:    runtime_id of its new location
         """
-        self._fwd_table[thread_id] = new_location
+        self._fwd_table[thread_uid] = new_location
+
+    def get_to_send_requests(self):
+        return self._get_list( self._status_req )
 
     def shutdown(self):
         # TODO: move all foreign threads to other runtimes
         pass
-
 
     def _get_list(self, queue):
         """ Return a list from a ThreadSafe Queue """
         list = [ ]
         while True:
             try:
-                list.append( queue(block=False) )
+                list.append( queue.get(block=False) )
             except Empty:
                 break
 
         return list
 
-    def _get_runtime_id(self, thread_id):
+    def _get_runtime_id(self, thread_uid):
         """ Return the id of the runtime that currently runs this thread """
-        if to not in self._fwd_table:
+        if thread_uid not in self._fwd_table:
             # TODO: Broadcast message to discover the location of the thread
-            print('ERROR: {} not in fwd table..'.format(to))
+            print('ERROR: {} not in fwd table..'.format(thread_uid))
             return None
 
-        return self._fwd_table[to]
+        return self._fwd_table[thread_uid]
